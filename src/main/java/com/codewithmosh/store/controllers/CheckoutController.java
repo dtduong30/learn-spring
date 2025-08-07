6a2320e9c7bb26c1c1b30fe3b7455787f79a14c4
Duong <duong.dangtuan@tribox.me>
2025-08-07 13:50:34 +0700

    Refactor and extract service checkout Add config bearer token for swagger

--- src/main/java/com/codewithmosh/store/controllers/CheckoutController.java
+++ src/main/java/com/codewithmosh/store/controllers/CheckoutController.java
@@ -2,61 +2,30 @@ package com.codewithmosh.store.controllers;
 
 import com.codewithmosh.store.dtos.CheckoutRequest;
 import com.codewithmosh.store.dtos.CheckoutResponse;
-import com.codewithmosh.store.entities.Order;
-import com.codewithmosh.store.entities.OrderItem;
-import com.codewithmosh.store.entities.OrderStatus;
-import com.codewithmosh.store.repositories.CartRepository;
-import com.codewithmosh.store.repositories.OrderRepository;
-import com.codewithmosh.store.services.AuthService;
-import com.codewithmosh.store.services.CartService;
+import com.codewithmosh.store.dtos.ErrorDto;
+import com.codewithmosh.store.exceptions.CartEmptyException;
+import com.codewithmosh.store.exceptions.CartNotFoundException;
+import com.codewithmosh.store.services.CheckoutService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/checkout")
 @AllArgsConstructor
 public class CheckoutController {
-  private final CartRepository cartRepository;
-  private final AuthService authService;
-  private final OrderRepository orderRepository;
-  private final CartService cartService;
-
-  @PostMapping("")
-  public ResponseEntity<?> checkout(@Valid @RequestBody CheckoutRequest request) {
-    var cart = cartRepository.getCartWithItems(request.getCartId()).orElse(null);
-    if (cart == null) {
-      return ResponseEntity.badRequest().body(Map.of("message", "Cart not found"));
-    }
-
-    if (cart.getItems().isEmpty()) {
-      return ResponseEntity.badRequest().body(Map.of("message", "Cart is empty"));
-    }
-
-    var order = new Order();
-    order.setTotalPrice(cart.getTotalPrice());
-    order.setStatus(OrderStatus.PENDING);
-    order.setCustomer(authService.getCurrentUser());
-
-    cart.getItems().forEach(item -> {
-      var orderItem = new OrderItem();
-      orderItem.setOrder(order);
-      orderItem.setQuantity(item.getQuantity());
-      orderItem.setProduct(item.getProduct());
-      orderItem.setTotalPrice(item.getTotalPrice());
-      orderItem.setUnitPrice(item.getProduct().getPrice());
-      order.getItems().add(orderItem);
-    });
-
-    orderRepository.save(order);
-    cartService.clearCart(cart.getId());
-
-    return ResponseEntity.ok(new CheckoutResponse(order.getId()));
-  }
+	private final CheckoutService checkoutService;
+	
+	@SecurityRequirement(name = "bearerAuth")
+	@PostMapping("")
+	public CheckoutResponse checkout(@Valid @RequestBody CheckoutRequest request) {
+		return checkoutService.checkout(request);
+	}
+	
+	@ExceptionHandler({CartNotFoundException.class, CartEmptyException.class})
+	public ResponseEntity<ErrorDto> handleCartException(RuntimeException e) {
+		return ResponseEntity.badRequest().body(new ErrorDto(e.getMessage()));
+	}
 }
