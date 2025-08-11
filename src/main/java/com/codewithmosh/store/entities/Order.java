8d2eea0b96e815f57cdec97a3e1740b967b9fca2
Duong <duong.dangtuan@tribox.me>
2025-08-11 10:30:19 +0700

    Get orders of customer

--- src/main/java/com/codewithmosh/store/entities/Order.java
+++ src/main/java/com/codewithmosh/store/entities/Order.java
@@ -14,40 +14,39 @@ import java.util.Set;
 @Entity
 @Table(name = "orders")
 public class Order {
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "id")
-  private Long id;
-
-  @ManyToOne()
-  @JoinColumn(name = "customer_id")
-  private User customer;
-
-  @Column(name = "status")
-  @Enumerated(EnumType.STRING)
-  private OrderStatus status;
-
-
-  @Column(name = "created_at", insertable = false, updatable = false)
-  private LocalDateTime createdAt;
-
-  @Column(name = "total_price")
-  private BigDecimal totalPrice;
-
-  @OneToMany(mappedBy = "order", cascade = CascadeType.PERSIST)
-  private Set<OrderItem> items = new LinkedHashSet<>();
-
-  public static Order fromCart(Cart cart, User customer) {
-    var order = new Order();
-    order.setCustomer(customer);
-    order.setTotalPrice(cart.getTotalPrice());
-    order.setStatus(OrderStatus.PENDING);
-
-    cart.getItems().forEach(item -> {
-      var orderItem = new OrderItem(order, item.getProduct(), item.getQuantity());
-      order.items.add(orderItem);
-    });
-
-    return order;
-  }
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	
+	@ManyToOne()
+	@JoinColumn(name = "customer_id")
+	private User customer;
+	
+	@Column(name = "status")
+	@Enumerated(EnumType.STRING)
+	private OrderStatus status;
+	
+	@Column(name = "created_at", insertable = false, updatable = false)
+	private LocalDateTime createdAt;
+	
+	@Column(name = "total_price")
+	private BigDecimal totalPrice;
+	
+	@OneToMany(mappedBy = "order", cascade = CascadeType.PERSIST)
+	private Set<OrderItem> items = new LinkedHashSet<>();
+	
+	public static Order fromCart(Cart cart, User customer) {
+		var order = new Order();
+		order.setCustomer(customer);
+		order.setTotalPrice(cart.getTotalPrice());
+		order.setStatus(OrderStatus.PENDING);
+		
+		cart.getItems().forEach(item -> {
+			var orderItem = new OrderItem(order, item.getProduct(), item.getQuantity());
+			order.items.add(orderItem);
+		});
+		
+		return order;
+	}
 }
