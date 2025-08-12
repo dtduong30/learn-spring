175bc142e91d5a0397b5731665fb8e9fa2dee789
Duong <duong.dangtuan@tribox.me>
2025-08-12 16:20:01 +0700

    Create checkout session

--- src/main/java/com/codewithmosh/store/controllers/CheckoutController.java
+++ src/main/java/com/codewithmosh/store/controllers/CheckoutController.java
@@ -1,14 +1,15 @@
 package com.codewithmosh.store.controllers;
 
 import com.codewithmosh.store.dtos.CheckoutRequest;
-import com.codewithmosh.store.dtos.CheckoutResponse;
 import com.codewithmosh.store.dtos.ErrorDto;
 import com.codewithmosh.store.exceptions.CartEmptyException;
 import com.codewithmosh.store.exceptions.CartNotFoundException;
 import com.codewithmosh.store.services.CheckoutService;
+import com.stripe.exception.StripeException;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,8 +21,14 @@ public class CheckoutController {
 	
 	@SecurityRequirement(name = "bearerAuth")
 	@PostMapping("")
-	public CheckoutResponse checkout(@Valid @RequestBody CheckoutRequest request) {
-		return checkoutService.checkout(request);
+	public ResponseEntity<?> checkout(@Valid @RequestBody CheckoutRequest request) {
+		try {
+			return ResponseEntity.ok(checkoutService.checkout(request));
+		} catch (StripeException e) {
+			return ResponseEntity
+					.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.body(new ErrorDto("Error creating a checkout session"));
+		}
 	}
 	
 	@ExceptionHandler({CartNotFoundException.class, CartEmptyException.class})
